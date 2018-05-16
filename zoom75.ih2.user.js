6fe7289aa653b59fbcf2a9cea19bd90c8f9d3377
Paul Wratt <paul.wratt@gmail.com>
2018-05-17 01:53:37 +1200

    release v1.80 - see ChangLog

--- zoom75.ih2.user.js
+++ zoom75.ih2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Zoom 75 - Imperial Hero II
 // @namespace    paulwratt.ih2
-// @version      1.70
+// @version      1.80
 // @description  For IH2 when using zoom 75%
 // @author       paulwratt [TsuKe_Morehu_X]
 // @homepage     https://paulwratt.github.io/imperial-hero-2-pwtools/
@@ -23,6 +23,8 @@
 
   if (location.href.indexOf('imperialhero.org/web/public/game') !== -1) {
     GM_addStyle('' +
+      '.widget.quest-tracker .quest-tracker-wrapper .quest-container { max-height: 500px; !important; }' +
+      '.widget.quest-tracker .quest-tracker-wrapper .quest-container .mCustomScrollBox { max-height: 500px; !important; }' +
       '.smelting-recipe-wrapper .recipe-info .info { width: 256px !important; }' +
       '.quest-wrapper .quest-desc .obj-box.small { width: 72px !important; }' +
       '.slot-inventory { width: 30px !important; }' +
